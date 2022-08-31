f719fa4b622da515782053e88dc30e2c8d9b66e6
Jose Palala <jose.palala@live.com>
2022-08-31 11:02:14 +1000

    import pokemon logo

---
+++ README.md
@@ -0,0 +1,6 @@
+POKEMON APP
+
+Basic react app
+
+
+All logos belong to their respective owners - © 2022 Pokémon. © 1995–2022 Nintendo/Creatures Inc./GAME FREAK inc. 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { hot } from 'react-hot-loader/root';
 import React, { Component } from "react";
 import "./App.css";
+import pokemonLogo from './images/pokemonLogo.png';
 class App extends Component {
 render() {
     return (
       <div className="App">
-        <header className="App-header">Test 🤗</header>
+        <header className="App-header"><img src={pokemonLogo} /></header>
       </div>
     );
   }
 }
-export default App;
+export default hot(App);
